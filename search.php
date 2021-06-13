5da2039792540e936ca28c9597b09954e1c6afde
axelP13 <axel.pelletier10@gmail.com>
2021-06-13 23:56:00 +0200

    j

--- search.php
+++ search.php
@@ -1,8 +1,7 @@
 <?php 
-$id = $_POST["id"];
-
-$json = file_get_contents('http://api.betaseries.com/search/shows?text='.$id.'&key=1d4db8125fb1&fields=id');
-$obj = json_decode($json);
-echo $obj->id;
-// http://api.betaseries.com/search/shows?text=the_queen%27s_gambit&key=1d4db8125fb1&fields=id
-?>7
+$id = isset($_POST["titre"]) ? $_POST["titre"] : NULL;
+$json = file_get_contents('http://api.betaseries.com/search/shows?key=1d4db8125fb1&text='.$id.'');
+$obj = json_decode($json, true);
+var_dump($obj['shows']);
+//http://api.betaseries.com/search/shows?text=the_queen%27s_gambit&key=1d4db8125fb1&fields=id
+?>
