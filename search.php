cc088c2e9d31eff493466e7cd65098bcc9e155b7
IT-Yohan <yohan.chapuis@gmail.com>
2021-06-13 20:14:47 +0200

    f

--- search.php
+++ search.php
@@ -1,8 +1,8 @@
 <?php 
 $id = $_POST["id"];
 
-$json = file_get_contents('url_here');
+$json = file_get_contents('http://api.betaseries.com/search/shows?text='.$id.'&key=1d4db8125fb1&fields=id');
 $obj = json_decode($json);
-echo $obj->access_token;
+echo $obj->id;
 // http://api.betaseries.com/search/shows?text=the_queen%27s_gambit&key=1d4db8125fb1&fields=id
 ?>7
