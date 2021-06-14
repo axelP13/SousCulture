8cc54077b75fdbe0def38ddf8960d2dd41922455
IT-Yohan <yohan.chapuis@gmail.com>
2021-06-14 20:32:29 +0200

    d

--- detail.php
+++ detail.php
@@ -16,13 +16,15 @@ $obj = json_decode($json); // on décode le json
 
 echo('
     <div class="container-fluid h-100">
+        </br>
+    
         <div class="row">
-            <div class="col-3">
+            <div class="col-4">
             </div>
-            <div class="col-3">
+            <div class="col-2">
                 <a href="./detail.php?id='.$obj->show->id.'"><img class="img-fluid" src="'.$obj->show->images->poster.'" alt="Image"></a>
             </div>
-            <div class="col-3">
+            <div class="col-2">
                 <div class="row h-100">
                     <a class ="my-auto mx-auto" href="./detail.php?id='.$obj->show->id.'">
                         <h2>'.$obj->show->title.'</h2>
@@ -30,15 +32,23 @@ echo('
                     <div>'.$obj->show->description.'</div>
                 </div>
             </div>
-            <div class="col-3">
+            <div class="col-4">
             </div>     
         </div>
-       </br>
-        <div class="row">
-            <div class="col min-height: 100%">
-                <textarea class="md-textarea form-control" spellcheck="true" name="texte" placeholder="Description du site" rows="5"></textarea>
+        </br>
+        </br>
+        <form name="form" method="POST" action="./nouveauFilm.php">
+            <div class="row">
+                <div class="col min-height: 100%">
+                    <textarea class="md-textarea form-control" spellcheck="true" name="texte" placeholder="Description du site" rows="5"></textarea>
+                </div>
             </div>
-        </div>
+            </br>
+            </br>
+            <div class="row d-flex flex-wrap align-items-center bg-info">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit"> Envoyer la critique </button>
+            </div>
+        </form>
     </div>
 ');
 
