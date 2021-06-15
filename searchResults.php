9af90677201e6baf5570f431900250075790f3e0
IT-Yohan <yohan.chapuis@gmail.com>
2021-06-15 06:11:46 +0200

    k

--- searchResults.php
+++ searchResults.php
@@ -5,16 +5,17 @@
 <link rel="stylesheet" href="./css/templatemo-sixteen.css">
 
 <?php 
-$type = $_POST["radio"];
-if($type == "show") {
+$type = $_POST["radio"]; // On récupère la valeur de "radio" envoyée grâce au formulaire de admin.html
+if($type == "show") { 
 
-$texte = isset($_POST["titre"]) ? $_POST["titre"] : NULL; // Façon d'écrire if/else
-$json = file_get_contents('http://api.betaseries.com/search/shows?key=1d4db8125fb1&text='.$texte.''); // On fetch le résultat
-$obj = json_decode($json); // on décode le json
+$texte = isset($_POST["titre"]) ? $_POST["titre"] : NULL; // Façon d'écrire if/else, vérifie la présence de titre dans les données POST
+$json = file_get_contents('http://api.betaseries.com/search/shows?key=1d4db8125fb1&text='.$texte.''); // On fetch le résultat de la requête 
+$obj = json_decode($json); // on décode le json en objet
 
     echo '<div class="container">
             <div class="row">';
-    foreach ($obj->shows as $show) {
+    foreach ($obj->shows as $show) { // Pour chaque élement de obj.shows que l'on nomme $show
+        //Echo affiche du html et les '.' servent à concaténer les variables avec le texte.
         echo ' 
             <div class="col-md-2">
                 <div class="product-item">
@@ -28,9 +29,10 @@ $obj = json_decode($json); // on décode le json
     };
 }
 
-else {
+else { //Si le titre n'est pas un show 
+
 $texte = isset($_POST["titre"]) ? $_POST["titre"] : NULL;
-$json = file_get_contents('http://api.betaseries.com/search/movies?key=1d4db8125fb1&text='.$texte.'');
+$json = file_get_contents('http://api.betaseries.com/search/movies?key=1d4db8125fb1&text='.$texte.''); // Même requête avec movies
 $obj = json_decode($json); // on décode le json
 
     echo '<div class="container">
